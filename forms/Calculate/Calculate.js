bd572a8d211505a0d1d25395e14382c78a0d3cd7
JuliaJo1 <Jlj26718@creighton.edu>
2020-10-05 16:16:37 -0500

    added notes to the forms

--- forms/Calculate/Calculate.js
+++ forms/Calculate/Calculate.js
@@ -1,14 +1,21 @@
  
+ /*Has a function named calcAvgSquare that can calculate the average of 3 numbers times the square of the first number. The formula is: 
+       ((num1 + num2 + num3)/3) * num12*/
+ 
  
  function calcAvgSquare (num1,num2,num3){
     return((num1+num2+num3)/3)*(num1*num1)
 }
+
+/*Gets 3 numbers from the user in the main program (not in the function). 
+Function parameter names and the argument names (used in main program when calling the function) CANNOT be the same. */
+  
 
 
 let num1 = Number(prompt("Enter the first number."))
 let num2 = Number(prompt("Enter the second number."))
 let num3 = Number(prompt("Enter the third number."))
 
-let answer = calcAvgSquare(num1,num2,num3)
-alert(`The calculated average squared is ${answer}`)
+  let answer = calcAvgSquare(num1,num2,num3)
+      alert(`The calculated average squared is ${answer}`)
 
--- forms/incomeBracket/incomeBracket.js
+++ forms/incomeBracket/incomeBracket.js
@@ -1,13 +1,22 @@
 btnSubmit.onclick=function(){
   let income = inptEnterIncome.value
+  
+  /*Used this format for output into a control using a template literal.
+    Example: say the user input:  100000. The output would be:
+             "With your income of $100000, you are in a tax bracket of 25%"*/
+  
   let bracket = `With your income of $${income}, you are in a tax bracket of `
     if (income < 30000) {
+    
+    
    lblTaxBracket.value = bracket + '8%'
      } else if (income < 99999 && income >= 30000) {
    lblTaxBracket.value = bracket + '15%'
     } else if (income >= 99999) {
    lblTaxBracket.value = bracket + '25%'
   }
+  
+  
 }
 btnGoAgain.onclick=function(){
   lblTaxBracket.value = "Enter a different income amount"
