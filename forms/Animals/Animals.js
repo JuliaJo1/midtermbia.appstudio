92f369aef7e4e566427824c825178902114a47d7
JuliaJo1 <Jlj26718@creighton.edu>
2020-10-05 15:42:49 -0500

    animals

---
+++ forms/Animals/Animals.js
@@ -0,0 +1,14 @@
+/*Created an array named myAnimals with these animals in it:
+   dog, cat, horse, meerkat*/
+
+let myAnimals = ['dog', 'cat', 'horse', 'meerkat']
+
+
+/* Output the last animal in the array using an array method to an alert using a template literal to the console with the format below. Used the myAnimals array in the template literal to show the last animal added: 
+*/
+
+lastAnimal = prompt('Please type a name of an animal')
+  myAnimals.push(lastAnimal)
+    alert(`The last animal is a/an ${lastAnimal}.`)
+
+
